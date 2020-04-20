40d0ce588e24e431d3a2dd68c8877de84bbd88ac
karinehf <karine@foss.sola.no>
2020-04-20 10:36:16 +0200

    Bruker deploy fra script-phase

--- push-to-aws.sh
+++ push-to-aws.sh
@@ -3,7 +3,6 @@ pip install awscli
 export PATH=$PATH:$HOME/.local/bin
 aws configure list
 eval $(aws ecr get-login --no-include-email --region eu-west-1) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-# aws ecr get-login-password | docker login  --password-stdin --username AWS "459994320734.dkr.ecr.eu-west-1.amazonaws.com"
 docker build -t app_image:latest .
-docker tag app_image:latest 459994320734.dkr.ecr.eu-west-1.amazonaws.com/app-image-repo
-docker push 459994320734.dkr.ecr.eu-west-1.amazonaws.com/app-image-repo:latest
+docker tag app_image:latest $AWS_ACCOUNT_ID.dkr.ecr.eu-west-1.amazonaws.com/app-image-repo
+docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-west-1.amazonaws.com/app-image-repo:latest
