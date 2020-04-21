b3e06745a9660777b2fd4df743cd81d37afe4e9d
karinehf <karine@foss.sola.no>
2020-04-21 14:48:39 +0200

    moving secrets 2

--- push-to-aws.sh
+++ push-to-aws.sh
@@ -10,15 +10,3 @@ docker tag $AWS_IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.am
 
 #Using Fargate/dynamic port mapping + load balancer
 #aws ecs update-service --cluster $AWS_CLUSTER --service $AWS_SERVICE --force-new-deployment
-
-
-
-#Keep a copy, so we can alter the above
-# - cd app
-# - pip install awscli
-# - export PATH=$PATH:$HOME/.local/bin
-# - eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-# - docker build -t app_image:latest .
-# - docker tag app_image:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/app-image-repo
-# - docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/app-image-repo:latest
-# - aws ecs update-service --cluster app-image-cluster --service app_image_service --force-new-deployment
