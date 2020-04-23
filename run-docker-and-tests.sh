d37a10ae0148d052de3d08d3af9763ff97d7fcf1
karinehf <karine@foss.sola.no>
2020-04-23 09:41:40 +0200

    Trying to see basename again

--- run-docker-and-tests.sh
+++ run-docker-and-tests.sh
@@ -21,6 +21,11 @@ docker ps
 docker container rm $(docker ps -a -q -f "name=app_container")
 cd -
 
+current_dir=$PWD
+app_name="${current_dir%"${current_dir##*[!/]}"}" # extglob-free multi-trailing-/ trim
+app_name="${app_name##*/}"
+printf "$app_name"
+
 ########
 #Simple python script can be run like this: 
 #python firsttest.py
