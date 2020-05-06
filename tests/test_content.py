5c899239112ce2bec3ee8dadcef1ff1490296563
karinehf <karine@foss.sola.no>
2020-05-06 15:37:40 +0200

    Think tests are not really checked

--- tests/test_content.py
+++ tests/test_content.py
@@ -7,7 +7,7 @@ class TestFlaskContent(unittest.TestCase):
     def test_hello_world(self):
         #Tests content of web app.
         hW = app.HelloWorld()
-        self.assertEqual(hW.get(), "Hello World!")
+        self.assertEqual(hW.get(), "Hello wwwworld!")
 
 if __name__ == '__main__':
     unittest.main()
