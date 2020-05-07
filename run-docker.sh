dd3bced5d5d15b4d155c36e310209bba24acc3b7
karinehf <karine@foss.sola.no>
2020-05-07 08:11:42 +0200

    Dividing the script phase

---
+++ run-docker.sh
@@ -0,0 +1,11 @@
+#Get container port
+PORT=$(python get_port.py)
+#Docker
+#Build image and run container
+cd app
+
+docker build -t app_image:test .
+docker run -d -p $PORT:$PORT --name app_container app_image:test
+
+docker ps
+cd -
