05df69f76b994b0121468b901ed7320fb8b61b11
karinehf <karine@foss.sola.no>
2020-05-07 12:24:42 +0200

    Trying running from script again

--- tests/test_content.py
+++ tests/test_content.py
@@ -7,7 +7,7 @@ class TestFlaskContent(unittest.TestCase):
     def test_hello_world(self):
         #Tests content of web app.
         hW = app.HelloWorld()
-        self.assertEqual(hW.get(), "Hello World!")
+        self.assertEqual(hW.get(), "Hello Wwworld!")
 
 if __name__ == '__main__':
     unittest.main()
